e63c8af10b7135ee1742852888bbd05df2054349
marinsgabriel1997 <marinsgabriel1997@gmail.com>
2025-01-22 20:43:50 +0000

    novo teste

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "renameTab") {
+    // Obtém a aba ativa e injeta um script nela para alterar o título
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
       if (activeTab) {
-        chrome.tabs.update(activeTab.id, { title: request.newTitle });
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          func: changeTitle,
+          args: [request.newTitle]
+        });
       }
     });
   }
 });
+
+function changeTitle(newTitle) {
+  document.title = newTitle;  // Altera o título da página
+}
